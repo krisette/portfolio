a0b042caa43d3007a4821802df2e55889c10aec6
Krisette Odegard <kmodeg@gmail.com>
2023-09-28 16:26:33 -0700

    feat: starting to build out the landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+  const myFont = localFont({
+    src: '/Metropolis-Regular.otf',
+    weight: '400',
+  })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   )
 }
